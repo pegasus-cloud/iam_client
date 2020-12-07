59b9c3a314c02dbd83a89bc43a6357cbddf0eafe
James3 Li(李麒傑) <james3_li@asus.com>
2020-12-07 09:59:32 +0800

    add default timeout for grpc connection

--- iam/provider.go
+++ iam/provider.go
@@ -17,9 +17,13 @@ func Init(provider PoolProvider) {
 		count:   len(provider.Hosts) * provider.ConnPerHost,
 		clients: make(chan client, len(provider.Hosts)*provider.ConnPerHost),
 	}
+	var timeout time.Duration = 5000
+	if provider.Timeout != 0 {
+		timeout = provider.Timeout
+	}
 	utility.RouteResponseType = provider.RouteRepsonseType
 	for i := 0; i < p.count; i++ {
-		c, err := grpc.Dial(provider.Hosts[i%len(provider.Hosts)], grpc.WithInsecure(), grpc.WithBlock(), grpc.WithTimeout(provider.Timeout*time.Millisecond))
+		c, err := grpc.Dial(provider.Hosts[i%len(provider.Hosts)], grpc.WithInsecure(), grpc.WithBlock(), grpc.WithTimeout(timeout*time.Millisecond))
 		if err != nil {
 			panic(err)
 		}
